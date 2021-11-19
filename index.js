a7043d129e7e489b42a58755b812d6cd68689b3e
Macc <2453170895@qq.com>
2021-11-19 14:47:27 +0800

    静态页面完成

---
+++ index.js
@@ -0,0 +1,11 @@
+const SENTENCE_API = 'https://v1.hitokoto.cn/';
+      THEME_COLORS = [
+        'FFFFCC',
+        '#CCFFFF',
+        '#FFCCCC',
+        '#FFCC99',
+        '#99CCCC',
+        '#99CC66',
+        '#666699',
+        '#FFFFCC'
+      ]
